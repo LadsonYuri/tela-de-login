60ab01acdf342ec8dcd6233645bc195a6097b3d0
Ladson Yuri <ladsonyurip@gmail.com>
2023-09-08 12:26:59 -0300

    first commit

---
+++ v1.py
@@ -0,0 +1,7 @@
+a=(input("Escreva seu User: "))
+b=(int(input("Escreva sua Password: ")))
+
+if(a == "admin" and b == 1234):
+    print("Bem Vindo!")
+else:
+    print("\n","Erro, senha ou usuario incorretos!")
